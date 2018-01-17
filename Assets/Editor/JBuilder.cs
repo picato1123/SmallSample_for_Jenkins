218821636fdda4c0f3ef3454558d6bf902378b13
TAKATOU-PC\hisat <hisato.t05241123@gmail.com>
2018-01-17 19:26:32 +0900

    Assets/Editor Assets/Editor/JBuilder.cs作成 スクリプトはブログとやや異なっていた。

---
+++ Assets/Editor/JBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class JBuilder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid()
+    {
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        List<string> allScene = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                allScene.Add(scene.path);
+            }
+        }
+        PlayerSettings.applicationIdentifier = "com.ht.sampleforjenkins";
+        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "BuildforJenkins.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
